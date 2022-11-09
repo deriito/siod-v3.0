2b9effd217d2217e538dda91d2b104d3f56ac887
Guang Yang <guangyangok@gmail.com>
2022-11-09 20:07:38 +0900

    オートマチックな段階的な手法(assert-dead(p))

--- assign_site_test2.scm
+++ assign_site_test2.scm
@@ -14,10 +14,17 @@
 (func 1)
 (gc)
 
-(func 1)
-(func 2)
-(func 1)
-(func 2)
-
 (func 2)
 (gc)
+
+
+
+;; Type: CONS(MyCons);
+;; Path to object: SYMBOL(func); ->
+;; CLOSURE; ->
+;; SYMBOL(global-val-a); ->
+;; CONS(MyCons);
+
+;; 上記のテストプログラムでは，
+;; SYMBOL(func) -> CLOSUREの代入操作が1回だけ実行するため，代入操作が発生する場所は記録不能，
+;; 同じように，CLOSURE; -> SYMBOL(global-val-a) / SYMBOL(global-val-b)も記録不能
